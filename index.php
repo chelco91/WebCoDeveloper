59b20474becdc5f9ff01577ae63dadc0ff07088d
Christopher Collopy <chelco91@gmail.com>
2015-09-03 21:48:33 -0600

    created initial landing section and project section

--- index.php
+++ index.php
@@ -7,25 +7,55 @@
 	</head>
 
 	<body class="sfooter">
-		<div class="container">
-			<header>
-				<?php require_once("lib/templates/header.php"); ?>
-			</header>
+		<header>
+			<?php require_once("lib/templates/header.php"); ?>
+		</header>
 
-			<!-- Projects Section -->
-			<section>
+		<!-- Landing Section -->
+		<section class="parallax" data-stellar-background-ratio="1.5" id="landing-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="jumbotron col-md-8 col-md-offset-2">
+							<h2>Hi I'm Meeeeeee!</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="welcome-text col-md-8 col-md-offset-2">
+							<p class="lead">Welcome to a new world</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
-			</section>
+		<!-- Projects Section -->
+		<section class="parallax pattern" data-stellar-background-ratio="0.5" id="project-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="enticers">
+							<?php require_once("lib/templates/enticers.php"); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
-			<!-- About Me Section -->
-			<section>
+		<!-- About Me Section -->
+		<section>
 
-			</section>
+		</section>
 
-			<!-- Contact Section -->
-			<section>
+		<!-- Contact Section -->
+		<section>
 
-			</section>
-		</div>
+		</section>
 	</body>
 </html>
