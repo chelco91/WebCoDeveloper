1b3f827bd3974b322bf421ddfaec6b637755ff15
Christopher Collopy <chelco91@gmail.com>
2015-09-03 20:41:30 -0600

    tagged the head-utils.php with require once in the index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php $PAGE_TITLE = "Webcodeveloper | Welcome";
+		require_once("lib/templates/head-utils.php"); ?>
+	</head>
+
+	<body>
+
+	</body>
+</html>
