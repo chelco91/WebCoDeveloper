b4eb59c4baee0623288b91c4b33de09cdf9f1b6a
Christopher Collopy <chelco91@gmail.com>
2015-09-06 13:34:36 -0600

    redid the organization of the rotational boxes template

--- lib/templates/rotational-boxes.php
+++ lib/templates/rotational-boxes.php
@@ -1,43 +1,35 @@
 <div id="enticers">
 	<div class="container">
 		<div class="enticer first">
-			<a href='#'>
+			<a href="">
 				<div class="content_holder">
-					<img class="enticer_image" src="lib/images/massive-project-bg.jpg"/>
-
-					<div class="text" style="background: #2d2d2e"><p>!!!! Major Project !!!!</p></div>
-				</div>
-				<!-- .content_holder -->
+					<img class="enticer_image" src="lib/images/massive-project-bg.jpg" />
+					<div class="text" style="background: #2d2d2e">
+						<p>Our entry deadline has now passed. One deserving Edmonton charity organization is about to become our winner. Keep a close watch as the winner will be announced this fall.&nbsp;</p>
+					</div>
+				</div><!-- .content_holder -->
 			</a>
-		</div>
-		<!-- .enticer -->
-
+		</div><!-- .enticer -->
 		<div class="enticer">
-			<a href='#'>
+			<a href="">
 				<div class="content_holder">
-					<img class="enticer_image" src="lib/images/small-project-1-bg.jpg"/>
-
-					<div class="text" style="background: #005583"><p class="p1">Small Project 1</p></div>
-				</div>
-				<!-- .content_holder -->
+					<img class="enticer_image" src="lib/images/small-project-1-bg.jpg" />
+					<div class="text" style="background: #005583">
+						<p class="p1">The award winning website for Crowsnest Pass. &nbsp;Showcasing natural beauty on a new, easy to update, and responsive municipal website.&nbsp;</p>
+					</div>
+				</div><!-- .content_holder -->
 			</a>
-		</div>
-		<!-- .enticer -->
-
+		</div><!-- .enticer -->
 		<div class="enticer">
-			<a href='#'>
+			<a href="">
 				<div class="content_holder">
-					<img class="enticer_image" src="lib/images/small-project-2-bg.jpg"/>
-
-					<div class="text"><p class="p1">Small Project 2</p></div>
-				</div>
-				<!-- .content_holder -->
+					<img class="enticer_image" src="lib/images/small-project-2-bg.jpg" />
+					<div class="text">
+						<p class="p1">Unifying the online presence of Alberta's fifth largest school board.</p>
+					</div>
+				</div><!-- .content_holder -->
 			</a>
-		</div>
-		<!-- .enticer -->
-
+		</div><!-- .enticer -->
 		<div class="clear"></div>
-	</div>
-	<!-- .container -->
-</div>
-<!-- #enticers -->
+	</div><!-- .container -->
+</div><!-- #enticers -->
