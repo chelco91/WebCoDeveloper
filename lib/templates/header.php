299b65675a0800fc1db04a7fc3ea72a1ec9f9e26
Christopher Collopy <chelco91@gmail.com>
2015-09-08 10:22:08 -0600

    took out projects from header nav

--- lib/templates/header.php
+++ lib/templates/header.php
@@ -8,7 +8,7 @@
 		</div>
 		<div class="collapse navbar-collapse" id="navbar-collapsed">
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#projects-section">Projects</a></li>
+<!--				<li><a href="#projects-section">Projects</a></li>-->
 				<li><a href="#about-section">About Me</a></li>
 				<li><a href="#contact-section">Contact</a></li>
 			</ul>
